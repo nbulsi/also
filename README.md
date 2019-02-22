301cdb167b234df91c16e854b243d571ae962442
Zhufei Chu <chuzhufei@nbu.edu.cn>
2019-02-22 09:42:53 +0800

    basic framework

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# koala
-Majority-based Logic synthesis Tool
+# also
+Advanced Logic Synthesis and  Optimization tool (ALSO)
+
+ALSO is based on the [EPFL Logic Synthesis Libraries](https://github.com/lsils/lstools-showcase), we aim to exploit advanced logic synthesis tools for both FPGA and emerging nanotechnologies.
+
+##Requirements
+A modern compiler is required to build the libraries. Compiled successfully with Clang 6.0.1, GCC 7.3.0, and GCC 8.2.0. 
+
+##How to Compile
+```bash
+git clone https://github.com/nbulsi/also.git
+cd also
+mkdir build
+cd build
+cmake ..
+make
+./build/bin/also
+```
+
---
+++ src/store.hpp
@@ -0,0 +1,219 @@
+/* also: Advanced Logic Synthesis and Optimization tool
+ * Copyright (C) 2019- Ningbo University, Ningbo, China 
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef STORE_HPP
+#define STORE_HPP
+
+#include <alice/alice.hpp>
+#include <mockturtle/mockturtle.hpp>
+#include <mockturtle/io/write_verilog.hpp>
+#include <fmt/format.h>
+#include <kitty/kitty.hpp>
+
+using namespace mockturtle;
+
+namespace alice
+{
+
+  /********************************************************************
+   * Genral stores                                                    *
+   ********************************************************************/
+
+  /* aiger */
+  ALICE_ADD_STORE( aig_network, "aig", "a", "AIG", "AIGs" )
+
+  ALICE_PRINT_STORE( aig_network, os, element )
+  {
+    os << "AIG PI/PO = " << element.num_pis() << "/" << element.num_pos() << "\n";
+  }
+
+  ALICE_DESCRIBE_STORE( aig_network, element )
+  {
+    return fmt::format( "{} nodes", element.size() );
+  }
+
+  /* mig */
+  ALICE_ADD_STORE( mig_network, "mig", "m", "MIG", "MIGs" )
+
+  ALICE_PRINT_STORE( mig_network, os, element )
+  {
+    os << "MIG PI/PO = " << element.num_pis() << "/" << element.num_pos() << "\n";
+  }
+  
+  ALICE_DESCRIBE_STORE( mig_network, element )
+  {
+    return fmt::format( "{} nodes", element.size() );
+  }
+
+  /* xmg */
+  ALICE_ADD_STORE( xmg_network, "xmg", "x", "xmg", "xmgs" )
+
+  ALICE_PRINT_STORE( xmg_network, os, element )
+  {
+    os << fmt::format( " xmg i/o = {}/{} gates = {} ", element.num_pis(), element.num_pos(), element.num_gates() );
+    os << "\n";
+    //os << "xmg PI/PO = " << element.num_pis() << "/" << element.num_pos() << "\n";
+  }
+  
+  ALICE_DESCRIBE_STORE( xmg_network, element )
+  {
+    return fmt::format( "{} nodes", element.size() );
+  }
+  /* opt_network */
+
+  class optimum_network
+  {
+    public:
+      optimum_network() = default;
+
+      optimum_network( const kitty::dynamic_truth_table& function )
+        : function( function ) {}
+
+      optimum_network( kitty::dynamic_truth_table&& function )
+        : function( std::move( function ) ) {}
+
+      bool exists() const
+      {
+        static std::vector<std::unordered_set<kitty::dynamic_truth_table, kitty::hash<kitty::dynamic_truth_table>>> hash;
+
+        if ( function.num_vars() >= hash.size() )
+        {
+          hash.resize( function.num_vars() + 1 );
+        }
+
+        return !hash[function.num_vars()].insert( function ).second;
+      }
+
+    public: /* field access */
+      kitty::dynamic_truth_table function{0};
+      std::string network;
+  };
+
+  ALICE_ADD_STORE( optimum_network, "opt", "o", "network", "networks" )
+
+    ALICE_DESCRIBE_STORE( optimum_network, opt )
+    {
+      if ( opt.network.empty() )
+      {
+        return fmt::format( "{}", kitty::to_hex( opt.function ) );
+      }
+      else
+      {
+        return fmt::format( "{}, optimum network computed", kitty::to_hex( opt.function ) );
+      }
+    }
+
+  ALICE_PRINT_STORE( optimum_network, os, opt )
+  {
+    os << fmt::format( "function (hex): {}\nfunction (bin): {}\n", kitty::to_hex( opt.function ), kitty::to_binary( opt.function ) );
+
+    if ( opt.network.empty() )
+    {
+      os << "no optimum network computed\n";
+    }
+    else
+    {
+      os << fmt::format( "optimum network: {}\n", opt.network );
+    }
+  }
+
+  /********************************************************************
+   * Read and Write                                                   *
+   ********************************************************************/
+  ALICE_ADD_FILE_TYPE( aiger, "Aiger" );
+
+  ALICE_READ_FILE( aig_network, aiger, filename, cmd )
+  {
+    aig_network aig;
+    lorina::read_aiger( filename, mockturtle::aiger_reader( aig ) );
+    return aig;
+  }
+  
+  ALICE_ADD_FILE_TYPE( verilog, "Verilog" );
+  
+  ALICE_READ_FILE( mig_network, verilog, filename, cmd )
+  {
+    mig_network mig;
+    lorina::read_verilog( filename, mockturtle::verilog_reader( mig ) );
+    return mig;
+  }
+  
+  ALICE_WRITE_FILE( mig_network, verilog, mig, filename, cmd )
+  {
+     mockturtle::write_verilog( mig, filename );
+  }
+  
+  ALICE_READ_FILE( xmg_network, verilog, filename, cmd )
+  {
+    xmg_network xmg;
+    lorina::read_verilog( filename, mockturtle::verilog_reader( xmg ) );
+    return xmg;
+  }
+
+  ALICE_WRITE_FILE( xmg_network, verilog, xmg, filename, cmd )
+  {
+     mockturtle::write_verilog( xmg, filename );
+  }
+
+  ALICE_ADD_FILE_TYPE( bench, "BENCH" );
+  
+  ALICE_WRITE_FILE( xmg_network, bench, xmg, filename, cmd )
+  {
+     mockturtle::write_bench( xmg, filename );
+  }
+
+  /********************************************************************
+   * Convert from aig to mig                                          *
+   ********************************************************************/
+  ALICE_CONVERT( aig_network, element, mig_network )
+  {
+    aig_network aig = element;
+
+    /* LUT mapping */
+    mapping_view<aig_network, true> mapped_aig{aig};
+    lut_mapping_params ps;
+    ps.cut_enumeration_ps.cut_size = 4;
+    lut_mapping<mapping_view<aig_network, true>, true>( mapped_aig, ps );
+
+    /* collapse into k-LUT network */
+    const auto klut = *collapse_mapped_network<klut_network>( mapped_aig );
+
+    /* node resynthesis */
+    mig_npn_resynthesis resyn;
+    auto mig = node_resynthesis<mig_network>( klut, resyn );
+
+    /* cut rewriting */
+    cut_rewriting_params cut_ps;
+    cut_ps.cut_enumeration_ps.cut_size = 4;
+    cut_rewriting( mig, resyn, cut_ps );
+    mig = cleanup_dangling( mig );
+
+    return mig;
+  }
+
+
+}
+
+#endif
