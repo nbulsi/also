3dd4f5b226e391505f0911af7579be5b846f1264
Zhufei Chu <chuzhufei@nbu.edu.cn>
2019-04-10 22:48:23 +0800

    xmg cost based on QCA

--- src/commands/xmgcost.hpp
+++ src/commands/xmgcost.hpp
@@ -37,6 +37,26 @@ namespace alice
       {
         xmg_network xmg = store<xmg_network>().current();
         
+        num_maj = 0;
+        num_xor = 0;
+        /* compute num_maj and num_xor */
+        xmg.foreach_gate( [&]( auto n ) 
+            {
+              if( xmg.is_maj( n ) )
+              {
+                num_maj++;
+              }
+              else if( xmg.is_xor3( n ) )
+              {
+                num_xor++;
+              }
+              else
+              {
+                assert( false && "only support XMG now" );
+              }
+            }
+            );
+
         num_gates = xmg.num_gates();
         num_inv = mockturtle::num_inverters( xmg );
 
@@ -45,22 +65,25 @@ namespace alice
 
         mockturtle::depth_view depth_xmg2{xmg, true};
         depth_mixed = depth_xmg2.depth();
-        std::tie( depth_maj, depth_inv ) = split_critical_path( depth_xmg2 );
+        std::tie( depth_maj, depth_inv, depth_xor ) = split_critical_path( depth_xmg2 );
 
         num_dangling = mockturtle::num_dangling_inputs( xmg );
 
-        qca_area = num_gates * 0.0012 + num_inv * 0.004;
-        qca_delay = depth_maj * 0.004 + depth_inv * 0.014;
-        qca_energy = num_gates * 2.94 + num_inv * 9.8;
+        qca_area = num_maj * 0.0012 + num_inv * 0.004 + num_xor * 0.0027;
+        qca_delay = depth_maj * 0.004 + depth_inv * 0.014 + depth_xor * 0.009;
+        qca_energy = num_maj * 2.94 + num_inv * 9.8 + num_xor * 6.615;
 
         env->out() << fmt::format( "[i] Gates             = {}\n"
+            "[i] Num MAJs          = {}\n"
+            "[i] Num XORs          = {}\n"
             "[i] Inverters         = {}\n"
             "[i] Depth (def.)      = {}\n"
             "[i] Depth mixed       = {}\n"
             "[i] Depth mixed (MAJ) = {}\n"
             "[i] Depth mixed (INV) = {}\n"
+            "[i] Depth mixed (XOR) = {}\n"
             "[i] Dangling inputs   = {}\n",
-            num_gates, num_inv, depth, depth_mixed, depth_maj, depth_inv, num_dangling );
+            num_gates, num_maj, num_xor, num_inv, depth, depth_mixed, depth_maj, depth_inv, depth_xor, num_dangling );
 
         env->out() << fmt::format( "[i] QCA (area)        = {:.2f} um^2\n"
             "[i] QCA (delay)       = {:.2f} ns\n"
@@ -70,28 +93,28 @@ namespace alice
 
       private:
       template<class Ntk>
-        std::pair<unsigned, unsigned> split_critical_path( Ntk const& ntk )
+        std::tuple<unsigned, unsigned, unsigned> split_critical_path( Ntk const& ntk )
         {
           using namespace mockturtle;
 
-          unsigned num_maj{0}, num_inv{0};
+          unsigned num_maj{0}, num_inv{0}, num_xor{0};
 
           node<Ntk> cp_node;
           ntk.foreach_po( [&]( auto const& f ) {
               auto level = ntk.level( ntk.get_node( f ) );
               if ( ntk.is_complemented( f ) )
               {
-              level++;
+                level++;
               }
 
               if ( level == ntk.depth() )
               {
-              if ( ntk.is_complemented( f ) )
-              {
-              ++num_inv;
-              }
-              cp_node = ntk.get_node( f );
-              return false;
+                if ( ntk.is_complemented( f ) )
+                {
+                ++num_inv;
+                }
+                cp_node = ntk.get_node( f );
+                return false;
               }
 
               return true;
@@ -99,33 +122,44 @@ namespace alice
 
           while ( !ntk.is_constant( cp_node ) && !ntk.is_pi( cp_node ) )
           {
-            num_maj++;
+            if( ntk.is_maj( cp_node ) )
+            {
+              num_maj++;
+            }
+            else if( ntk.is_xor3( cp_node ) )
+            {
+              num_xor++;
+            }
+            else
+            {
+              assert( false && "only support XMG now" );
+            }
 
             ntk.foreach_fanin( cp_node, [&]( auto const& f ) {
                 auto level = ntk.level( ntk.get_node( f ) );
                 if ( ntk.is_complemented( f ) )
                 {
-                level++;
+                  level++;
                 }
 
                 if ( level + 1 == ntk.level( cp_node ) )
                 {
-                if ( ntk.is_complemented( f ) )
-                {
-                num_inv++;
-                }
-                cp_node = ntk.get_node( f );
-                return false;
+                  if ( ntk.is_complemented( f ) )
+                  {
+                  num_inv++;
+                  }
+                  cp_node = ntk.get_node( f );
+                  return false;
                 }
                 return true;
                 } );
           }
 
-          return {num_maj, num_inv};
+          return {num_maj, num_inv, num_xor};
         }
       
       private:
-      unsigned num_gates{0u}, num_inv{0u}, depth{0u}, depth_mixed{0u}, depth_maj{0u}, depth_inv{0u}, num_dangling{0u};
+      unsigned num_gates{0u}, num_inv{0u}, num_maj{0u}, num_xor{0u}, depth{0u}, depth_mixed{0u}, depth_maj{0u}, depth_inv{0u}, depth_xor{0u}, num_dangling{0u};
       double qca_area, qca_delay, qca_energy;
   };
 
