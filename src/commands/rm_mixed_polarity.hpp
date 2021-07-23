75fe08095c6090d16da928583618b53b868d0a4e
zhw <69035409+zhw-zhw@users.noreply.github.com>
2021-07-23 22:06:57 +0800

    Create rm_mixed_polarity.hpp

---
+++ src/commands/rm_mixed_polarity.hpp
@@ -0,0 +1,59 @@
+#ifndef RM_MIXED_POLARITY_HPP
+#define RM_MIXED_POLARITY_HPP
+
+#include"../core/rm_mixed_polarity.hpp"
+
+namespace alice
+{
+
+  class rm_mixed_polarity_command : public command
+  {
+    public:
+      explicit rm_mixed_polarity_command( const environment::ptr& env ) : command( env, "Create a command, try hard!" )
+      {
+         add_flag( "--xag, -g",  "RM logic optimization for xag network" );
+         add_flag( "--xmg, -x",  "RM logic optimization for xmg network" );
+      }
+      
+      void execute()
+      {
+      
+        if( is_set( "xag" ) )
+        {
+      	 	  mockturtle::xag_network xag=store<xag_network>().current();                         
+              
+              
+            depth_view depth_xag1( xag );
+  	        rm_mixed_polarity(depth_xag1);             
+            xag = cleanup_dangling( xag );
+       
+            store<xag_network>().extend(); 
+            store<xag_network>().current() = xag;
+         }
+         else if( is_set( "xmg" ) )
+         {
+      	 	  mockturtle::xmg_network xmg=store<xmg_network>().current();                         
+              
+              
+            depth_view depth_xmg1( xmg );
+  	        rm_mixed_polarity(depth_xmg1);             
+            xmg = cleanup_dangling( xmg );
+       
+            store<xmg_network>().extend(); 
+            store<xmg_network>().current() = xmg;
+         }
+     	 
+	    }
+
+
+    private:
+        int strategy = 0;
+  };
+
+  ALICE_ADD_COMMAND( rm_mixed_polarity, "Rewriting" )
+//这里表示生成的命令在rewriting下面
+
+}
+
+#endif
+
