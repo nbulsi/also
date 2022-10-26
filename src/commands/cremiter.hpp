c8736ccb6090ddff52d6b343c73f9bc37b6219d5
Zhufei Chu <chuzhufei@nbu.edu.cn>
2022-10-26 10:45:22 +0800

    xmg/xmg miter

--- src/commands/cremiter.hpp
+++ src/commands/cremiter.hpp
@@ -34,6 +34,17 @@ protected:
     /* equivalence checking results */
     std::optional<bool> result;
     mockturtle::stopwatch<>::duration time{0};
+    mockturtle::xmg_network xmg_ref;
+    mockturtle::xmg_network xmg_impl;
+    
+    if( is_set( "miter_for_xmg" ) )
+    {
+      std::cout << "[warning] Most recent two XMGs will be derived\n";
+      auto xmg_store_size = store<xmg_network>().size();
+      assert( xmg_store_size >= 2u );
+      xmg_impl = store<xmg_network>()[ xmg_store_size - 1 ];
+      xmg_ref  = store<xmg_network>()[ xmg_store_size - 2 ];
+    }
 
     if ( is_set( "partial_miter" ) )
     {
@@ -41,6 +52,8 @@ protected:
       unsigned fail = 0u;
       unsigned unknown = 0u;
       std::vector<unsigned> unknown_po_list;
+
+      mockturtle::xmg_network xmg_miter;
 
       mockturtle::call_with_stopwatch( time, [&]() {
             
@@ -54,7 +67,20 @@ protected:
                 mockturtle::stopwatch<>::duration iteration_time{0};
                 
                 mockturtle::call_with_stopwatch( iteration_time, [&]() {
-                const auto miter = *mockturtle::pmiter<xmg_network>( aig, xmg, i );
+                if( is_set( "miter_for_xmg" ) )
+                {
+                  xmg_miter = *mockturtle::pmiter<xmg_network>( xmg_ref, xmg_impl, i );
+                }
+                else
+                {
+                  xmg_miter = *mockturtle::pmiter<xmg_network>( aig, xmg, i );
+                }
+
+                if( is_set( "verbose" ) )
+                {
+                  std::cout << fmt::format( "[i] {:6d} gates in miter for PO {:4d}, ", xmg_miter.num_gates(), i );
+                }
+
                 if( is_set( "enable_ec" ) ) 
                 {
                   equivalence_checking_params ps;
@@ -62,7 +88,7 @@ protected:
                   
                   ps.conflict_limit = conflict_limit;
 
-                  result = mockturtle::equivalence_checking( miter, ps, &st );
+                  result = mockturtle::equivalence_checking( xmg_miter, ps, &st );
 
                   if( result == std::nullopt )
                   {
@@ -70,7 +96,7 @@ protected:
                     unknown_po_list.push_back( i );
                     if( is_set( "verbose" ) ) 
                     {
-                      std::cout << "[i] UNKNOWN ";
+                      std::cout << "UNKNOWN, ";
                     }
                   }
                   else
@@ -80,7 +106,7 @@ protected:
                       success++;
                       if( is_set( "verbose" ) ) 
                       {
-                        std::cout << "[i] EQU "; 
+                        std::cout << "EQU, "; 
                       }
                     }
                     else
@@ -88,7 +114,7 @@ protected:
                       fail++;
                       if( is_set( "verbose" ) ) 
                       {
-                        std::cout << "[i] NEQ " << std::endl;
+                        std::cout << "NEQ, " << std::endl;
                       }
                       return;
                     }
@@ -98,8 +124,7 @@ protected:
                 
                 if( is_set( "verbose" ) ) 
                 {
-                  std::cout << fmt::format( "for node {}, iteration time: {:5.4f} seconds\n", 
-                      i, mockturtle::to_seconds( iteration_time ) ); 
+                  std::cout << fmt::format( "iteration time: {:5.4f} seconds\n", mockturtle::to_seconds( iteration_time ) ); 
                 }
             }
           } );
@@ -108,7 +133,17 @@ protected:
 
           if( unknown_po_list.size() > 0u )
           {
-            const auto new_miter = *mockturtle::pmiter_w_po_list<xmg_network>( aig, xmg, unknown_po_list );
+            mockturtle::xmg_network new_miter;
+            
+            if( is_set( "miter_for_xmg" ) )
+            {
+              new_miter = *mockturtle::pmiter_w_po_list<xmg_network>( xmg_ref, xmg_impl, unknown_po_list );
+            }
+            else
+            {
+              new_miter = *mockturtle::pmiter_w_po_list<xmg_network>( aig, xmg, unknown_po_list );
+            }
+            
             store<xmg_network>().extend();
             store<xmg_network>().current() = new_miter;
           }
@@ -130,21 +165,18 @@ protected:
     }
     else if ( is_set( "miter_for_xmg" ) )
     {
-      auto xmg_store_size = store<xmg_network>().size();
-      assert( xmg_store_size >= 2u );
-      auto xmg1 = store<xmg_network>()[ xmg_store_size - 1 ];
-      auto xmg2 = store<xmg_network>()[ xmg_store_size - 2 ];
-
       mockturtle::call_with_stopwatch( time, [&]() {
-      const auto miter = *mockturtle::miter<xmg_network>( xmg1, xmg2 );
+
+      const auto miter = *mockturtle::miter<xmg_network>( xmg_ref, xmg_impl );
+
       if( is_set( "enable_ec" ) ) {
       result = mockturtle::equivalence_checking( miter ); }
           
-          if( is_set( "new_entry" ) )
-          {
-            store<xmg_network>().extend();
-            store<xmg_network>().current() = miter;
-          }
+      if( is_set( "new_entry" ) )
+      {
+        store<xmg_network>().extend();
+        store<xmg_network>().current() = miter;
+      }
       } );
     }
     else if ( is_set( "miter_for_aig" ) )
