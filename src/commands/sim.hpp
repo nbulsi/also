998d8d971fac294d0c274fe23198f375c5df5c91
ZhufeiChu <chuzhufei@nbu.edu.cn>
2022-10-27 22:12:59 +0800

    partial simulator

--- src/commands/sim.hpp
+++ src/commands/sim.hpp
@@ -23,15 +23,52 @@ namespace alice
     public:
       explicit sim_command( const environment::ptr& env ) : command( env, "logic network simulation" )
       {
+        add_flag( "-a, --aig_network", " simulate current aig network" );
+        add_flag( "-x, --xmg_network", " simulate current xmg network" );
+        add_flag( "-p, --partial_simulate", " simulate current logic network using partial simulator " );
       }
       
     protected:
       void execute()
       {
-        xmg_network xmg = store<xmg_network>().current();
-        default_simulator<kitty::dynamic_truth_table> sim( xmg.num_pis() );
-        const auto tt = simulate<kitty::dynamic_truth_table>( xmg, sim )[0];
-        std::cout << "tt: " << tt._bits[0] << std::endl;
+        if( is_set( "xmg_network" ) )
+        {
+          if( is_set( "partial_simulate" ) )
+          {
+            xmg_network xmg = store<xmg_network>().current();
+            std::vector<kitty::partial_truth_table> pats( xmg.num_pis() );
+            for( auto i = 0; i < xmg.num_pis(); i++ )
+            {
+              pats[i].add_bits( 0x12345678, 32 );
+            }
+            partial_simulator sim( pats );
+
+            unordered_node_map<kitty::partial_truth_table, xmg_network> node_to_value( xmg );
+            simulate_nodes( xmg, node_to_value, sim );
+            
+            xmg.foreach_po( [&]( auto const& f ) {
+                std::cout << "tt: 0x" << ( xmg.is_complemented( f ) ? ~node_to_value[f] : node_to_value[f] )._bits[0] << std::endl; 
+                } );
+          }
+          else
+          {
+            xmg_network xmg = store<xmg_network>().current();
+            default_simulator<kitty::dynamic_truth_table> sim( xmg.num_pis() );
+            const auto tt = simulate<kitty::dynamic_truth_table>( xmg, sim )[0];
+            std::cout << "tt: 0x" << tt._bits[0] << std::endl;
+          }  
+        }
+        else if( is_set( "aig_network" ) )
+        {
+          aig_network aig = store<aig_network>().current();
+          default_simulator<kitty::dynamic_truth_table> sim( aig.num_pis() );
+          const auto tt = simulate<kitty::dynamic_truth_table>( aig, sim )[0];
+          std::cout << "tt: " << tt._bits[0] << std::endl;
+        }
+        else
+        {
+          std::cout << "At least one store should be specified, see 'sim -h' for help. \n";
+        }
       }
   };
 
